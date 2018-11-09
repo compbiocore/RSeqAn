615a4d9d46fb8b7f2b328078890dcbd0f27ca4d7
August Guang <august.guang@gmail.com>
2018-11-09 14:24:26 -0500

    changes to charstring

--- src/charstring.cpp
+++ src/charstring.cpp
@@ -22,3 +22,20 @@ namespace Rcpp {
       return s;
     };
 }
+
+
+using namespace seqan;
+
+//' containment test
+//' @export
+// [[Rcpp::export]]
+Rcpp::String containment_test(SEXP s1) {
+    Rcpp::Rcout << "Converting from SEXP to CharString" << std::endl;
+    seqan::CharString s = Rcpp::as<seqan::CharString>(s1);
+  
+    Rcpp::Rcout << "Running output test with toCString(CharString)" << std::endl;
+    Rcpp::Rcout << toCString(s) << std::endl;
+  
+    Rcpp::Rcout << "Converting from CharString to Rcpp::String" << std::endl;
+    return(Rcpp::wrap(s));
+  }
